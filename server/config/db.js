aeff713afee2387d18e4457ad5b5d499cf2669ab
rahulmakwana1 <rahulmakwana3535@gmail.com>
2021-10-18 18:51:17 -0400

    portfolio

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+
+module.exports =
+{
+    //"URI" : "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://rahul:bSwMh8ur8rjMr3iN@cluster0.ho1yl.mongodb.net/contact_list?retryWrites=true&w=majority",
+    "Secret" : 'PortfolioSecretKey'
+}
